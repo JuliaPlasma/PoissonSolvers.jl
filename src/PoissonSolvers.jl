d5a20fda486516d89d7cb64cd47f29e86fceb32d
Michael Kraus <michael.kraus@ipp.mpg.de>
2022-06-27 12:12:22 +0200

    Move Poisson solver code over from ParticleMethods.jl.

--- src/PoissonSolvers.jl
+++ src/PoissonSolvers.jl
@@ -1,5 +1,20 @@
 module PoissonSolvers
 
-# Write your package code here.
+include("poisson.jl")
+
+export PoissonSolver
+export solve!, eval_density, eval_potential, eval_field
+
+
+include("poisson_fft.jl")
+
+export PoissonSolverFFT
+
+
+include("splines.jl")
+include("poisson_splines.jl")
+
+export PoissonSolverPBSplines
+
 
 end
