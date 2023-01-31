91d2067721ac0274ec449048469bb63a6c565694
Michael Kraus <michael.kraus@ipp.mpg.de>
2023-01-31 14:48:49 +0100

    Extend and fix FFT solver.

--- src/poisson_fft.jl
+++ src/poisson_fft.jl
@@ -1,43 +1,38 @@
 
+using BSplineKit.Splines: PeriodicVector
 using FFTW
-using StatsBase
 
 
 fftmod(x, domain) = mod(x - domain[begin], domain[end] - domain[begin]) + domain[begin]
 
+
 struct FFTWBasis{DT, GT <: AbstractVector{DT}}
     domain::Tuple{DT,DT}
-    xgrid::GT
+    xgrid::PeriodicVector{DT,GT}
     Δx::DT
 end
 
 function FFTWBasis(domain, ngrid)
-    xgrid = range(domain[begin], domain[end], ngrid + 1)
+    xgrid = PeriodicVector(range(domain[begin], domain[end], ngrid + 1))
     Δx = xgrid[2] - xgrid[1]
     FFTWBasis(domain, xgrid, Δx)
 end
 
 PeriodicBasisFFTW(domain, n) = FFTWBasis(domain, n)
 
+Base.length(basis::FFTWBasis) = length(basis.xgrid) - 1
 
-function get_indices(b::FFTWBasis, x)
-    y = fftmod(x, b.domain) - b.domain[begin]
 
-    i1 = floor(Int, y / b.Δx)
-    i2 = mod( ceil(Int, y / b.Δx) - 1, length(b)) + 1
-
-    i1 == 0 && (i1 = length(b))
-    i2 == 0 && (i2 = length(b))
-
-    i1 == length(b)+1 && (i1 = 1)
-    i2 == length(b)+1 && (i2 = 1)
-
-    return (i1, i2)
+function nearest_indices(b::FFTWBasis, x)
+    i1 = floor(Int, (x - b.domain[begin]) / b.Δx) + 1
+    i2 = i1 + 1
+    return (mod(i1-1, length(b)) + 1, mod(i2-1, length(b)) + 1)
 end
 
-function get_index(b::FFTWBasis, x)
-    i1, i2 = get_indices(b, x)
-    return (abs(b.xgrid[i1] - x) ≤ abs(b.xgrid[i2] - x) ? i1 : i2)
+function nearest_index(b::FFTWBasis, x)
+    i1, i2 = nearest_indices(b, x)
+    i = (abs(b.xgrid[i1] - fftmod(x, b.domain)) ≤ abs(b.xgrid[i2] - fftmod(x, b.domain)) ? i1 : i2)
+    return i
 end
 
 
@@ -47,7 +42,7 @@ struct FFTWSolution{CT, BT}
 end
 
 function (s::FFTWSolution)(x::Number)
-    s.coefficients[get_index(s.basis, x)]
+    s.coefficients[nearest_index(s.basis, x)]
 end
 
 PoissonSolution(basis::FFTWBasis, coeffs::AbstractVector) = FFTWSolution(basis, coeffs)
@@ -58,18 +53,36 @@ function evalsolution(basis::FFTWBasis, coeffs::AbstractVector, x::Real)
 end
 
 
+struct FFTWDerivative{N, ST <: FFTWSolution}
+    solution::ST
+    function FFTWDerivative{N}(solution::ST) where {N,ST}
+        new{N,ST}(solution)
+    end
+end
+
+Base.:*(::Derivative{0}, s::FFTWSolution) = s
+Base.:*(::Derivative{N}, s::FFTWSolution) where {N} = FFTWDerivative{N}(s)
+
+Base.diff(s::FFTWSolution, op = Derivative(1)) = op * s
+
+function (d::FFTWDerivative{1})(x::Number)
+    i1, i2 = nearest_indices(d.solution.basis, x)
+    return (d.solution.coefficients[i2] - d.solution.coefficients[i1]) / d.solution.basis.Δx
+end
+
+
 struct PoissonSolverFFT{DT, BT <: FFTWBasis{DT}} <: PoissonSolver{DT}
     basis::BT
 end
 
-Base.length(p::PoissonSolverFFT) = length(p.basis.xgrid) - 1
+Base.length(p::PoissonSolverFFT) = length(p.basis)
 
 PoissonSolver(basis::FFTWBasis) = PoissonSolverFFT(basis)
 
 function solve!(coeffs::AbstractVector, p::PoissonSolverFFT, rhs::AbstractVector)
     ρ̂ = rfft(rhs)
     k² = [(i - 1)^2 for i in eachindex(ρ̂)]
-    ϕ̂ = -ρ̂ ./ k²
+    ϕ̂ = ρ̂ ./ k²
     ϕ̂[1] = 0
     ϕ̂ ./= ( 2π / (p.basis.domain[end] - p.basis.domain[begin]) )^2
     coeffs .= irfft(ϕ̂, length(rhs))
--- test/fft_tests.jl
+++ test/fft_tests.jl
@@ -1,30 +1,13 @@
 using PoissonSolvers
+using PoissonSolvers: nearest_index, nearest_indices
 
 ngrid = 64
-
-domain = (-2π, +2π)
-
-
-sol(x) = + sin(x)
-rhs(x) = - sin(x)
-
-b = FFTWBasis(domain, ngrid)
-s = PoissonSolverFFT(b)
-ρ = rhs.(b.xgrid[1:end-1])
-ϕ = sol.(b.xgrid[1:end-1])
-φ = solve(s, ρ)
-p = Potential(b, rhs)
-
-@test s == PoissonSolver(b)
-
-@test φ ≈ ϕ
-@test φ == p.coefficients
 
 
 domain = (0.0, 1.0)
 
-sol(x) = + sin(2π * x)
-rhs(x) = - 4π^2 * sin(2π * x)
+sol(x) = sin(2π * x)
+rhs(x) = 4π^2 * sin(2π * x)
 
 b = FFTWBasis(domain, ngrid)
 s = PoissonSolverFFT(b)
@@ -37,3 +20,40 @@ p = Potential(b, rhs)
 
 @test φ ≈ ϕ
 @test φ == p.coefficients
+
+@test nearest_index(b, domain[begin]) == 1
+@test nearest_index(b, domain[end]) == 1
+
+for x in (0.1, 0.25, 0.333333, 0.5)
+    i,j = nearest_indices(b, x)
+    @test b.xgrid[i] ≤ x
+    @test b.xgrid[j] ≥ x
+end
+
+
+domain = (-2π, +2π)
+
+sol(x) = sin(x)
+rhs(x) = sin(x)
+
+b = FFTWBasis(domain, ngrid)
+s = PoissonSolverFFT(b)
+ρ = rhs.(b.xgrid[1:end-1])
+ϕ = sol.(b.xgrid[1:end-1])
+φ = solve(s, ρ)
+p = Potential(b, rhs)
+
+@test s == PoissonSolver(b)
+
+@test φ ≈ ϕ
+@test φ == p.coefficients
+
+@test nearest_index(b, domain[begin]) == 1
+@test nearest_index(b, domain[end]) == 1
+
+for x in (-4.0, -3.14, -0.5, 0.0, +0.5, +3.14, +4.0)
+    i,j = nearest_indices(b, x)
+    # println(" x = $x, i = $i, j = $j, xi = $(b.xgrid[i]), xj = $(b.xgrid[j])")
+    @test b.xgrid[i] ≤ x
+    @test b.xgrid[j] ≥ x
+end
