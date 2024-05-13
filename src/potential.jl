9bbb27e4bc6c5bb310a38d4a66b9fb1101d2dabf
Michael Kraus <michael.kraus@ipp.mpg.de>
2024-05-13 15:05:16 +0200

    Store RHS in Potential struct.

--- src/potential.jl
+++ src/potential.jl
@@ -4,13 +4,14 @@ struct Potential{BT, CT <: AbstractVector, PT, ST <: PoissonSolver}
     coefficients::CT
     potential::PT
     solver::ST
+    rhs::CT
 
     function Potential(basis, init_rhs) where {DT}
         solver = PoissonSolver(basis)
         coeffs = solve(solver, init_rhs)
         potential = PoissonSolution(basis, coeffs)
 
-        new{typeof(basis), typeof(coeffs), typeof(potential), typeof(solver)}(basis, coeffs, potential, solver)
+        new{typeof(basis), typeof(coeffs), typeof(potential), typeof(solver)}(basis, coeffs, potential, solver, zero(coeffs))
     end
 end
 
@@ -22,8 +23,9 @@ Potential(basis) = Potential(basis, zeros(length(basis)))
 (p::Potential)(x::Number, derivative::Derivative) = (derivative * p.potential)(x)
 (p::Potential)(x::AbstractArray, derivative::Derivative) = (derivative * p.potential).(x)
 
+rhs(p::Potential) = p.rhs
 
-function update!(p::Potential, rhs)
+function update!(p::Potential, rhs = p.rhs)
     solve!(p.coefficients, p.solver, rhs)
     return p
 end
